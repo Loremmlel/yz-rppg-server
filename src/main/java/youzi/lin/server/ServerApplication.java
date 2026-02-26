de0e1d63cce8a0703c5aa00b313e6fd31ce7b216
柚子 <loremmlel@163.com>
2026-02-26 19:36:05 +0800

    init

---
+++ src/main/java/youzi/lin/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package youzi.lin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
